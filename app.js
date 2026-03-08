b0da1088b68e8fe976630b35b2d1f94511f0312d
realyasirdev <yasiralikhanrazakhail@gmail.com>
2026-03-08 11:52:18 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,7 @@
+let age = prompt("Apni age enter karo:");
+
+let totalWeeks = 90 * 52;
+let usedWeeks = age * 52;
+let weeksLeft = totalWeeks - usedWeeks;
+
+console.log(`You have ${weeksLeft} weeks left.`);
